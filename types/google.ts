31594bc37f15918bd66e2bccda332b4fc2ca351e
Dohyeon Yun <yundohyun050121@gmail.com>
2024-05-02 10:04:06 +0900

    add: `types`

---
+++ types/google.ts
@@ -0,0 +1,15 @@
+export type GoogleToken = {
+  access_token: string;
+  refresh_token: string;
+};
+
+export type GoogleUserInfo = {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+  locale: string;
+};
